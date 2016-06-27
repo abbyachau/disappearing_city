1f937d31478b7a4c42f9bb0a4b9c08686cbbe0bf
abbyachau <abbyachau@gmail.com>
2016-06-26 22:28:21 -0700

    adds working cities params

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,8 +1,10 @@
 class CitiesController < ApplicationController
   def index
+    @cities = City.all
   end
 
   def show
+    @city = City.find params[:id]
   end
 
   def edit
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :cities
   get 'test', to: 'static_pages#test', as: 'test'
-
+  get '/cities/:id', to: 'cities#show'
   root 'static_pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
